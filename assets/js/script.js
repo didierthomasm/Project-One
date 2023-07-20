ca50365938d24bc9fd7c514f449d0518b5a4d2b1
rarg08 <rarg08@gmail.com>
2023-07-19 20:00:46 -0600

    Coding changes to include the images when the date is changed
    added js code to show the image in the carousel, added a dynamic date so when the date changes the image and prompts from the APIs also update.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,84 +1,69 @@
-// Sample images for the carousel
-const carouselImages = [
-    "image1.jpg",
-    "image2.jpg",
-    "image3.jpg",
-    // Add more image filenames here...
-];
-
-// Function to create the carousel with images
-function createCarousel() {
-    const carouselDiv = document.querySelector(".carousel");
-
-    carouselImages.forEach((image) => {
-        const img = document.createElement("img");
-        img.src = image;
-        carouselDiv.appendChild(img);
-    });
-
-    let slideIndex = 0;
-
-    function showSlide(index) {
-        const slides = document.querySelectorAll(".carousel img");
-        slides.forEach((slide) => (slide.style.display = "none"));
-        slides[index].style.display = "block";
-    }
-
-    function nextSlide() {
-        slideIndex++;
-        if (slideIndex >= carouselImages.length) {
-            slideIndex = 0;
-        }
-        showSlide(slideIndex);
-    }
-
-    // Set up the carousel to rotate images every 5 seconds
-    setInterval(nextSlide, 5000);
-}
-
 // Function to initialize the date picker
 function initializeDatePicker() {
-    const datepicker = document.getElementById("datepicker");
-    // You can use a date picker library or create a custom one here
+  const datepicker = document.getElementById("datepicker");
 
-    // For simplicity, let's use the built-in HTML5 date input
-    // This will display a native date picker on supporting browsers
-    datepicker.type = "date";
+  // Set a default date for the date picker (e.g., today's date)
+  const today = new Date();
+  const defaultDate = today.toISOString().split('T')[0];
+  datepicker.value = defaultDate;
+
+  datepicker.addEventListener("change", (event) => {
+      fetchData();
+  });
+
+  // For simplicity, let's use the built-in HTML5 date input
+  // This will display a native date picker on supporting browsers
+  datepicker.type = "date";
 }
 
 // Call the functions to create the carousel and initialize the date picker
 createCarousel();
 initializeDatePicker();
 
+// Sample images for the carousel
+const carouselImages= src="data.url";
+
+// Function to create the carousel with images
+function createCarousel() {
+  // ... your carousel creation code ...
+}
 
 //CALLING APIS OF APOD AND NEOW
 function fetchData() {
+  var key = 'DhZgqHPR8sd2nvKgECz74jcTRRxDcioHOCvgtd7z';
+  var datepicker = document.getElementById("datepicker");
+  var selectedDate = datepicker.value;
 
-    var key = 'DhZgqHPR8sd2nvKgECz74jcTRRxDcioHOCvgtd7z';
-    var getNeow = `https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-08-11&end_date=2017-08-11&api_key=${key}`;
-    
-    fetch(getNeow)
+  var getNeow = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${selectedDate}&end_date=${selectedDate}&api_key=${key}`;
+  var getApod = `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${selectedDate}`;
+
+  const carouselDiv = document.querySelector(".carousel");
+
+  fetch(getNeow)
       .then(function (response) {
-        return response.json();
+          return response.json();
       })
       .then(function (data) {
-        console.log(data);
+          console.log("NEOW Data:", data);
       })
-  }
-    fetchData();
-  
-  
-  function fetchData2() {
-  
-      var api_key = 'DhZgqHPR8sd2nvKgECz74jcTRRxDcioHOCvgtd7z';
-      var getApod = `https://api.nasa.gov/planetary/apod?api_key=${api_key}&start_date=2017-07-01&end_date=2017-07-11`;
-      
-      fetch(getApod)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
+      .catch(function (error) {
+          console.log("Error fetching NEOW data:", error);
       });
-  }
-    fetchData2();
+
+  fetch(getApod)
+      .then(function (response) {
+          return response.json();
+      })
+      .then(function (data) {
+          console.log("APOD Data:", data);
+          var imgURL = data.url;
+          const img = document.createElement("img");
+          img.src = imgURL;
+          console.log(img);
+          carouselDiv.innerHTML = ''; // Clear any existing images in the carousel
+          carouselDiv.appendChild(img);
+      })
+      .catch(function (error) {
+          console.log("Error fetching APOD data:", error);
+      });
+}
