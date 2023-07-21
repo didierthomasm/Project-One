3413c5fc46b202c59afe954fd89820928aee17b8
didierthomasm <didierthomas.m@gmail.com>
2023-07-20 20:29:50 -0600

    setting local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,33 @@
+// Get local storage
+function getLocalStorage() {
+    let localStorageDate =  localStorage.getItem('dates');
+    let dates = JSON.parse(localStorageDate);
+    console.log(dates);
+}
+
+getLocalStorage();
+
+// Set date in local storage
+function setLocalStorage(date) {
+    let dateArray = JSON.parse(localStorage.getItem('dates')) || [];
+    if (!dateArray.includes(date)) {
+        console.log(dateArray.length);
+        if (dateArray.length < 5) {
+            console.log(`Estoy a tope, no me eches más... borra uno antes ${dateArray}`)
+            dateArray.push(date);
+            localStorage.setItem('dates', JSON.stringify(dateArray));
+        }
+    }
+}
+
 // Function to initialize the date picker
 function initializeDatePicker() {
     const datepicker = document.getElementById("datepicker");
     // Set a default date for the date picker (e.g., today’s date)
-    const today = new Date();
-    const defaultDate = today.toISOString().split('T')[0];
-    datepicker.value = defaultDate;
-    datepicker.addEventListener("change", (event) => {
-        fetchData();
+    datepicker.value = new Date().toISOString().split('T')[0];
+    datepicker.addEventListener("change", () => {
+        setLocalStorage(datepicker.value);
+        fetchData(datepicker.value);
     });
     // For simplicity, let’s use the built-in HTML5 date input
     // This will display a native date picker on supporting browsers
@@ -14,13 +35,10 @@ function initializeDatePicker() {
 }
 
 // Function to fetch data from the API and display it in the carousel
-function fetchData() {
-    var key = 'DhZgqHPR8sd2nvKgECz74jcTRRxDcioHOCvgtd7z';
-    var datepicker = document.getElementById("datepicker");
-    var selectedDate = datepicker.value;
-    var getNeow = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${selectedDate}&end_date=${selectedDate}&api_key=${key}`;
-    var getApod = `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${selectedDate}`;
-    const carouselDiv = document.getElementById("carousel");
+function fetchData(date) {
+    const key = 'DhZgqHPR8sd2nvKgECz74jcTRRxDcioHOCvgtd7z';
+    let getNeow = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${date}&end_date=${date}&api_key=${key}`;
+    let getApod = `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${date}`;
 
     fetch(getNeow)
         .then(function (response) {
@@ -28,6 +46,8 @@ function fetchData() {
         })
         .then(function (data) {
             console.log("NEOW Data:", data);
+            factOfTheDay(data);
+            return data;
         })
         .catch(function (error) {
             console.log("Error fetching NEOW data:", error);
@@ -39,23 +59,31 @@ function fetchData() {
         })
         .then(function (data) {
             console.log("APOD Data:", data);
-            var imgURL = data.url;
-            var explan = data.explanation;
-            var title = data.title;
-            const img = document.createElement("img");
-            img.src = imgURL;
-            const explanationP = document.createElement("p");
-            explanationP.textContent = explan;
-            const titleHeader = document.createElement("h1");
-            titleHeader.textContent = title;
-            carouselDiv.innerHTML = ''; // Clear any existing content in the carousel
-            carouselDiv.appendChild(img); // Append the image to the carousel
-            carouselDiv.appendChild(titleHeader);
-            carouselDiv.appendChild(explanationP); // Append the explanation paragraph to the carousel
+            carruselElement(data);
+            return data;
+
         })
         .catch(function (error) {
             console.log("Error fetching APOD data:", error);
         });
+}
+
+function carruselElement(dataApod) {
+    const carouselElement = document.getElementById("carousel");
+    
+    const carruselDiv = document.createElement('div');
+    carruselDiv.classList.add('carruselDiv');
+    carruselDiv.innerHTML = `
+        <h2>${dataApod.title}</h2>
+        <img src="${dataApod.url}" alt="${dataApod.title}">
+        <p>${dataApod.explanation}</p>        
+    `
+    carouselElement.innerHTML = ''; // Clear any existing content in the carousel
+    carouselElement.appendChild(carruselDiv);
+}
+
+function factOfTheDay(dataNeow) {
+    
 }
 
 // Call the function to initialize the date picker
