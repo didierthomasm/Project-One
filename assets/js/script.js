b2fb5939bc2ce78073303bc29d86fc9956e0246c
iderobina1 <iderobina@gmail.com>
2023-07-20 20:34:24 -0600

    Revert "index"
    This reverts commit de2220850a44eca4105e6a98b8dea4adebb8b069.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,84 @@
+// Sample images for the carousel
 const carouselImages = [
-  "image1.jpg",
-  "image2.jpg",
-  "image3.jpg",
-  // Add more image filenames here...
+    "image1.jpg",
+    "image2.jpg",
+    "image3.jpg",
+    // Add more image filenames here...
 ];
 
+// Function to create the carousel with images
 function createCarousel() {
-  const carouselDiv = document.querySelector(".carousel");
+    const carouselDiv = document.querySelector(".carousel");
 
-  carouselImages.forEach((image, index) => {
-      const img = document.createElement("img");
-      img.src = image;
-      carouselDiv.appendChild(img);
+    carouselImages.forEach((image) => {
+        const img = document.createElement("img");
+        img.src = image;
+        carouselDiv.appendChild(img);
+    });
 
-      const button = document.createElement("button");
-      button.innerText = "Show Image " + (index + 1);
-      carouselDiv.appendChild(button);
-  });
+    let slideIndex = 0;
 
-  const buttons = document.querySelectorAll(".carousel button");
-  buttons.forEach((button, index) => {
-      button.addEventListener("click", () => {
-          showSlide(index);
-      });
-  });
+    function showSlide(index) {
+        const slides = document.querySelectorAll(".carousel img");
+        slides.forEach((slide) => (slide.style.display = "none"));
+        slides[index].style.display = "block";
+    }
 
-  let slideIndex = 0;
+    function nextSlide() {
+        slideIndex++;
+        if (slideIndex >= carouselImages.length) {
+            slideIndex = 0;
+        }
+        showSlide(slideIndex);
+    }
 
-  function showSlide(index) {
-      const slides = document.querySelectorAll(".carousel img");
-      const buttons = document.querySelectorAll(".carousel button");
-
-      slides.forEach((slide) => (slide.style.display = "none"));
-      buttons.forEach((button) => (button.style.display = "none"));
-
-      slides[index].style.display = "block";
-      buttons[index].style.display = "block";
-  }
-
-  setInterval(() => {
-      slideIndex++;
-      if (slideIndex >= carouselImages.length) {
-          slideIndex = 0;
-      }
-      showSlide(slideIndex);
-  }, 5000);
+    // Set up the carousel to rotate images every 5 seconds
+    setInterval(nextSlide, 5000);
 }
 
+// Function to initialize the date picker
 function initializeDatePicker() {
-  const datepicker = document.getElementById("datepicker");
-  datepicker.type = "date";
+    const datepicker = document.getElementById("datepicker");
+    // You can use a date picker library or create a custom one here
+
+    // For simplicity, let's use the built-in HTML5 date input
+    // This will display a native date picker on supporting browsers
+    datepicker.type = "date";
 }
 
+// Call the functions to create the carousel and initialize the date picker
 createCarousel();
 initializeDatePicker();
+
+
+//CALLING APIS OF APOD AND NEOW
+function fetchData() {
+
+    var key = 'DhZgqHPR8sd2nvKgECz74jcTRRxDcioHOCvgtd7z';
+    var getNeow = `https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-08-11&end_date=2017-08-11&api_key=${key}`;
+    
+    fetch(getNeow)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      })
+  }
+    fetchData();
+  
+  
+  function fetchData2() {
+  
+      var api_key = 'DhZgqHPR8sd2nvKgECz74jcTRRxDcioHOCvgtd7z';
+      var getApod = `https://api.nasa.gov/planetary/apod?api_key=${api_key}&start_date=2017-07-01&end_date=2017-07-11`;
+      
+      fetch(getApod)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      });
+  }
+    fetchData2();
